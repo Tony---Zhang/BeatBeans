077377afb1782bebc035ce277add4e14fc8bc580
Liu Jing <jing.liu@rea-group.com>
2017-05-18 18:43:35 +0800

    add event for change role

---
+++ Assets/SignPeterController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class SignPeterController : MonoBehaviour {
+
+	public void OnPointerEvent(bool isFocus) {
+		if (isFocus) {
+			Console.WriteLine ("Focus");
+		} else {
+			Console.WriteLine ("Blur");
+		}
+	}
+}
