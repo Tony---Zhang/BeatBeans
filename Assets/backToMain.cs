916e8cd675b4342e2bfb023de24e0d2403aa94fd
JingZhao <jingzhao@thoughtworks.com>
2017-05-19 12:42:44 +0800

    Add back scripts

---
+++ Assets/backToMain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backToMain : MonoBehaviour {
+
+	void Update () {
+		if (GvrController.AppButtonUp) {
+			SceneManager.LoadScene("BeatBeans", LoadSceneMode.Single);
+		}
+	}
+}
